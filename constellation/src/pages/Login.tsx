d0164615fd3381be8cfa8e151849526528a71b8f
최다희 <chdh8987@naver.com>
2024-10-25 17:18:54 +0900

    🌱  feat: 로그인 화면 추가

---
+++ constellation/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import Kakao from '../assets/kakao_Icon.png'
+
+function Login(){
+    return (
+        <>
+            <div>로그인..</div>
+            <img src={Kakao} alt='kakao-login-Icon'/>
+        </>
+    )
+}
+
+
+export default Login;
